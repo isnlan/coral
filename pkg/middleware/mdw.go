074388bb724a0c04f53884c303c5bfc646d7f8fd
snlansky <snlan@live.cn>
2020-08-26 16:23:24 +0800

    update cors

--- pkg/middleware/mdw.go
+++ pkg/middleware/mdw.go
@@ -176,13 +176,25 @@ func HandleNotFound(c *gin.Context) {
 
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		var isAccess = true
-		if isAccess {
-			// 核心处理方式
-			c.Header("Access-Control-Allow-Origin", "*")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
-			c.Set("content-type", "application/json")
+		origin := c.Request.Header.Get("Origin") //请求头部
+		if origin != "" {
+			//接收客户端发送的origin （重要！）
+			c.Header("Access-Control-Allow-Origin", origin)
+			//服务器支持的所有跨域请求的方法
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+			//允许跨域设置可以返回其他子段，可以自定义字段
+			c.Header("Access-Control-Allow-Headers", "Authorization, Content-Length, X-CSRF-Token, Token,session")
+			// 允许浏览器（客户端）可以解析的头部 （重要）
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+			//设置缓存时间
+			c.Header("Access-Control-Max-Age", "172800")
+			//允许客户端传递校验信息比如 cookie (重要)
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
+
+		//允许类型校验
+		if c.Request.Method == "OPTIONS" {
+			c.JSON(http.StatusOK, "ok!")
 		}
 
 		c.Next()
