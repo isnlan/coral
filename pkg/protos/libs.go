3d3ab1892dc10b9e03c39a62ac8e76bdc4e90806
snlansky <snlan@live.cn>
2020-12-25 14:03:26 +0800

    update

--- pkg/protos/libs.go
+++ pkg/protos/libs.go
@@ -1,3 +1,3 @@
 package protos
 
-//go:generate protoc -I protos/ protos/behavior.proto protos/auth.proto --go_out=plugins=grpc:protos
+//go:generate protoc --go_out=plugins=grpc:. behavior.proto auth.proto vm.proto
