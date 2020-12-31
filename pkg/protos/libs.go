1ed5f5e53897761971df9e354f523ebb850cf5ae
snlansky <snlan@live.cn>
2020-12-31 17:30:33 +0800

    add network.proto

--- pkg/protos/libs.go
+++ pkg/protos/libs.go
@@ -1,3 +1,3 @@
 package protos
 
-//go:generate protoc --go_out=plugins=grpc:. behavior.proto auth.proto vm.proto
+//go:generate protoc --go_out=plugins=grpc:. behavior.proto auth.proto vm.proto network.proto
