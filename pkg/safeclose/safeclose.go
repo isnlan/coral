2762a4ef743fc6cf95e9dfeb7d87df1364a0bc5e
snlansky <snlan@live.cn>
2021-03-12 22:50:21 +0800

    add safeclose

---
+++ pkg/safeclose/safeclose.go
@@ -0,0 +1,28 @@
+package safeclose
+
+type Close struct {
+	C chan struct{}
+}
+
+func New() *Close {
+	return &Close{C: make(chan struct{})}
+}
+
+func (c *Close) Close(f func() error) error  {
+	select {
+	case <- c.C:
+		return nil
+	default:
+		close(c.C)
+		return f()
+	}
+}
+
+func (c *Close) IsClosed() bool {
+	select {
+	case <-c.C:
+		return true
+	default:
+		return false
+	}
+}
