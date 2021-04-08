056f7401d6dafb85a933bb44f59a9997bd3dcd9c
snlansky <snlan@live.cn>
2021-04-08 16:41:16 +0800

    update

--- pkg/utils/request.go
+++ pkg/utils/request.go
@@ -42,6 +42,28 @@ func NewFileUploadRequest(uri string, params map[string]string, paramName, path 
 	return req, err
 }
 
+func NewFormDataRequest(uri string, params map[string]string) (*http.Request, error) {
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+
+	for key, val := range params {
+		_ = writer.WriteField(key, val)
+	}
+
+	err := writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", uri, body)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	return req, err
+}
+
 func NewWriterUploadRequest(uri string, params map[string]string, paramName, fileName string, src io.Reader) (*http.Request, error) {
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
