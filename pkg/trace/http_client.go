bfe8f7bd947ba069cbe5d2e05b640d7ddbc42b8c
snlansky <snlan@live.cn>
2021-04-25 14:28:37 +0800

    fix bug

--- pkg/trace/http_client.go
+++ pkg/trace/http_client.go
@@ -4,7 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/opentracing/opentracing-go"
@@ -17,9 +19,9 @@ func DoRequest(ctx context.Context, tr *http.Transport, req *http.Request, v int
 
 	var client *http.Client
 	if tr != nil {
-		client = &http.Client{Transport: tr}
+		client = &http.Client{Transport: &nethttp.Transport{RoundTripper: tr}}
 	} else {
-		client = http.DefaultClient
+		client = &http.Client{Transport: &nethttp.Transport{}}
 	}
 
 	res, err := client.Do(req)
@@ -49,4 +51,20 @@ func DoPost(ctx context.Context, url string, req interface{}, v interface{}) err
 		return err
 	}
 	return nil
+}
+
+func DefaultTransport() *http.Transport {
+	return &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	}
 }
