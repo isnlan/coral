d756ab99dde420b9071e6689aba88f4eb6f58c3a
snlansky <snlan@live.cn>
2021-06-03 11:11:51 +0800

    update

--- pkg/blink/lease/lease.go
+++ pkg/blink/lease/lease.go
@@ -1,33 +1,33 @@
 package lease
 
 type ChainLease struct {
-	NetworkID   string `json:"network_id"`
-	NetworkType string `json:"network_type"`
-	NetworkName string `json:"network_name"`
-	Account     string `json:"account"`
-	Team        string `json:"team"`
-	IsRunning   bool   `json:"is_running"`
-	Status      string `json:"status"`
-	TlsEnabled  bool   `json:"tls_enabled"`
+	NetworkID   string `json:"network_id" mapstructure:"network_id"`
+	NetworkType string `json:"network_type" mapstructure:"network_type"`
+	NetworkName string `json:"network_name" mapstructure:"network_name"`
+	Account     string `json:"account" mapstructure:"account"`
+	Team        string `json:"team" mapstructure:"team"`
+	IsRunning   bool   `json:"is_running" mapstructure:"is_running"`
+	Status      string `json:"status" mapstructure:"status"`
+	TlsEnabled  bool   `json:"tls_enabled" mapstructure:"tls_enabled"`
 }
 
 type ChannelLease struct {
-	ID         string `json:"id"`
-	NetworkID  string `json:"network_id"`
-	Name       string `json:"name"`
-	Endpoint   string `json:"endpoint"`
-	IsRunning  bool   `json:"status"`
-	SyncEnable bool   `json:"sync_enable"`
-	SyncDB     string `json:"sync_db"`
+	ID         string `json:"id" mapstructure:"id"`
+	NetworkID  string `json:"network_id" mapstructure:"network_id"`
+	Name       string `json:"name" mapstructure:"name"`
+	Endpoint   string `json:"endpoint" mapstructure:"endpoint"`
+	IsRunning  bool   `json:"status" mapstructure:"status"`
+	SyncEnable bool   `json:"sync_enable" mapstructure:"sync_enable"`
+	SyncDB     string `json:"sync_db" mapstructure:"sync_db"`
 }
 
 type AclLease struct {
-	ID           string `json:"id"`            // 应用ID
-	Name         string `json:"name"`          // 应用名称
-	ClientId     string `json:"client_id"`     // 客户端ID
-	ClientSecret string `json:"client_secret"` // 客户端Secret
-	Account      string `json:"account"`       // 创建账户
-	Team         string `json:"team"`          // 组
-	NetworkID    string `json:"network_id"`    // 网络ID（ChainID）
-	Enable       bool   `json:"enable"`        // 可用
+	ID           string `json:"id" mapstructure:"id"`                       // 应用ID
+	Name         string `json:"name" mapstructure:"name"`                   // 应用名称
+	ClientId     string `json:"client_id" mapstructure:"client_id"`         // 客户端ID
+	ClientSecret string `json:"client_secret" mapstructure:"client_secret"` // 客户端Secret
+	Account      string `json:"account" mapstructure:"account"`             // 创建账户
+	Team         string `json:"team" mapstructure:"team"`                   // 组
+	NetworkID    string `json:"network_id" mapstructure:"network_id"`       // 网络ID（ChainID）
+	Enable       bool   `json:"enable" mapstructure:"enable"`               // 可用
 }
