58919d5c0db55f75e026a0b74f9266020980fc9d
snlansky <snlan@live.cn>
2021-06-09 11:06:12 +0800

    update test

--- pkg/db/mongo_test.go
+++ pkg/db/mongo_test.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"reflect"
 	"testing"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"go.mongodb.org/mongo-driver/mongo"
 
@@ -64,5 +67,113 @@ func TestMongoDaoImpl(t *testing.T) {
 		fmt.Println(errors.Is(err, mongo.ErrNoDocuments))
 
 	})
+}
 
+func TestMongoApi(t *testing.T) {
+	mongoServer, err := memongo.Start("4.0.5")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer mongoServer.Stop()
+
+	client, err := MongoConnect("mongodb://admin:admin@127.0.0.1:27017/admin?authSource=admin")
+	assert.NoError(t, err)
+
+	coll := client.Database("blockchain_manager").Collection("api")
+
+	ctx := context.Background()
+	// id, err := primitive.ObjectIDFromHex("df05961e491bb6a77edeb7fc")
+	// assert.NoError(t, err)
+
+	var api0 API
+	err = FindOne(ctx, coll, map[string]interface{}{"_id": primitive.NewObjectID()}, &api0)
+	assert.True(t, errors.Is(err, mongo.ErrNoDocuments))
+
+	// var api API
+	// err = FindOne(ctx, coll, map[string]interface{}{"_id": id}, &api)
+	// assert.NoError(t, err)
+	// fmt.Println(api)
+
+	var list []*API
+	err = Find(ctx, coll, map[string]interface{}{}, 0, 0, primitive.D{}, func(cur *mongo.Cursor) error {
+		var api API
+		err := cur.Decode(&api)
+		if err != nil {
+			return err
+		}
+
+		list = append(list, &api)
+		return nil
+	})
+	assert.NoError(t, err)
+
+	fmt.Println(len(list))
+
+	// apiNew := &API{
+	// 	ID:             primitive.NewObjectID(),
+	// 	Scheme:         "s",
+	// 	Method:         "m",
+	// 	Path:           "p",
+	// 	AppName:        "",
+	// 	APIName:        "",
+	// 	APIType:        "",
+	// 	DocURL:         "",
+	// 	TotalCallCount: 0,
+	// 	ClientCount:    0,
+	// 	QPS:            0,
+	// 	HistoryMaxQPS:  0,
+	// 	SuccessCount:   0,
+	// 	TotalLatency:   0,
+	// }
+	// err = InsertOne(ctx, coll, apiNew)
+	// assert.NoError(t, err)
+
+	// api.Scheme = "update"
+	// err = UpdateOne(ctx, coll, map[string]interface{}{"_id": api.ID}, api)
+	// assert.NoError(t, err)
+	//
+	// c, err := Count(ctx, coll, map[string]interface{}{})
+	// assert.NoError(t, err)
+	// fmt.Println(c)
+	//
+	// err = DeleteOne(ctx, coll, map[string]interface{}{"_id": api.ID})
+	// assert.NoError(t, err)
+
+	err = DeleteMany(ctx, coll, map[string]interface{}{"path": "/api/v2/orgs"})
+	assert.NoError(t, err)
+
+	var filter []map[string]interface{}
+	filter = append(filter, map[string]interface{}{
+		"$group": map[string]interface{}{
+			"_id": "$app_name",
+			"count": map[string]interface{}{
+				"$sum": 1,
+			},
+		},
+	})
+
+	m := map[string]interface{}{}
+	res, err := Aggregations(ctx, coll, filter, reflect.TypeOf(m))
+	assert.NoError(t, err)
+
+	for _, item := range res {
+		fmt.Println(item)
+	}
+}
+
+type API struct {
+	ID             primitive.ObjectID `json:"id" bson:"_id"`                            // 主键ID
+	Scheme         string             `json:"scheme" bson:"scheme"`                     // http ws grpc
+	Method         string             `json:"method" bson:"method"`                     // http method
+	Path           string             `json:"path" bson:"path"`                         // http route path
+	AppName        string             `json:"app_name" bson:"app_name"`                 // application name
+	APIName        string             `json:"api_name" bson:"api_name"`                 // api 中文名称
+	APIType        string             `json:"api_type" bson:"api_type"`                 // api 接口类型
+	DocURL         string             `json:"doc_url" bson:"doc_url"`                   // 文档地址
+	TotalCallCount int64              `json:"total_call_count" bson:"total_call_count"` // 累计调用数量
+	ClientCount    int64              `json:"client_count" bson:"client_count"`         // 应用调用次数
+	QPS            int64              `json:"qps" bson:"qps"`                           // 当前qps
+	HistoryMaxQPS  int64              `json:"history_max_qps" bson:"history_max_qps"`   // 历史最高qps
+	SuccessCount   int64              `json:"success_count" bson:"success_count"`       // 成功次数
+	TotalLatency   int64              `json:"total_latency" bson:"total_latency"`       // 总耗时
 }
