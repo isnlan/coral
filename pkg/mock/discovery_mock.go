1de39c482b57fb9f0fc566ce49ecf6efc49551fc
snlansky <snlan@live.cn>
2021-06-29 11:54:14 +0800

    update mock test

--- pkg/mock/discovery_mock.go
+++ pkg/mock/discovery_mock.go
@@ -6,82 +6,19 @@ package mock
 
 import (
 	context "context"
-	reflect "reflect"
 
-	gomock "github.com/golang/mock/gomock"
 	"github.com/hashicorp/consul/api"
 	discovery "github.com/isnlan/coral/pkg/discovery"
 	grpc "google.golang.org/grpc"
 )
 
-// MockServiceDiscover is a mock of ServiceDiscover interface
-type MockServiceDiscover struct {
-	ctrl     *gomock.Controller
-	recorder *MockServiceDiscoverMockRecorder
-}
-
-// MockServiceDiscoverMockRecorder is the mock recorder for MockServiceDiscover
-type MockServiceDiscoverMockRecorder struct {
-	mock *MockServiceDiscover
-}
-
-// NewMockServiceDiscover creates a new mock instance
-func NewMockServiceDiscover(ctrl *gomock.Controller) *MockServiceDiscover {
-	mock := &MockServiceDiscover{ctrl: ctrl}
-	mock.recorder = &MockServiceDiscoverMockRecorder{mock}
-	return mock
-}
-
-// EXPECT returns an object that allows the caller to indicate expected use
-func (m *MockServiceDiscover) EXPECT() *MockServiceDiscoverMockRecorder {
-	return m.recorder
-}
-
-// RegisterHealthServer mocks base method
-func (m *MockServiceDiscover) RegisterHealthServer(s *grpc.Server) {
-	m.ctrl.T.Helper()
-	m.ctrl.Call(m, "RegisterHealthServer", s)
-}
-
-// RegisterHealthServer indicates an expected call of RegisterHealthServer
-func (mr *MockServiceDiscoverMockRecorder) RegisterHealthServer(s interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RegisterHealthServer", reflect.TypeOf((*MockServiceDiscover)(nil).RegisterHealthServer), s)
-}
-
-// ServiceRegister mocks base method
-func (m *MockServiceDiscover) ServiceRegister(name, address string, port int, tags ...string) (discovery.Deregister, error) {
-	m.ctrl.T.Helper()
-	varargs := []interface{}{name, address, port}
-	for _, a := range tags {
-		varargs = append(varargs, a)
-	}
-	ret := m.ctrl.Call(m, "ServiceRegister", varargs...)
-	ret0, _ := ret[0].(discovery.Deregister)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// ServiceRegister indicates an expected call of ServiceRegister
-func (mr *MockServiceDiscoverMockRecorder) ServiceRegister(name, address, port interface{}, tags ...interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	varargs := append([]interface{}{name, address, port}, tags...)
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ServiceRegister", reflect.TypeOf((*MockServiceDiscover)(nil).ServiceRegister), varargs...)
-}
-
-// WatchService mocks base method
-func (m *MockServiceDiscover) WatchService(ctx context.Context, name, tag string, ch chan<- []*discovery.ServiceInfo) {
-	m.ctrl.T.Helper()
-	m.ctrl.Call(m, "WatchService", ctx, name, tag, ch)
-}
-
-// WatchService indicates an expected call of WatchService
-func (mr *MockServiceDiscoverMockRecorder) WatchService(ctx, name, tag, ch interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "WatchService", reflect.TypeOf((*MockServiceDiscover)(nil).WatchService), ctx, name, tag, ch)
-}
+var _ discovery.ServiceDiscover = &MockServiceDiscover2{}
 
 type MockServiceDiscover2 struct {
+}
+
+func (s2 *MockServiceDiscover2) HTTPServiceRegister(name, address string, port int, tags ...string) (discovery.Deregister, error) {
+	return func() {}, nil
 }
 
 func (s2 *MockServiceDiscover2) RegisterHealthServer(s *grpc.Server) {
