431fb13ae307e969d8d2f11400a4ab6105c1b2cc
snlansky <snlan@live.cn>
2021-07-16 16:17:33 +0800

    update type name gen

--- pkg/utils/go.go
+++ pkg/utils/go.go
@@ -15,8 +15,25 @@ func SafeRun(f func() error) (err error) {
 	return f()
 }
 
+func elemName(elem reflect.Type) string {
+	if elem.Kind() == reflect.Ptr {
+		return elem.Elem().Name()
+	}
+
+	return elem.Name()
+}
+
 func MakeTypeName(tpy interface{}) string {
-	return reflect.TypeOf(tpy).Elem().Name()
+	tp := reflect.TypeOf(tpy)
+
+	switch tp.Kind() {
+	case reflect.Map:
+		return fmt.Sprintf("Map<%s:%s>", elemName(tp.Key()), elemName(tp.Elem()))
+	case reflect.Slice:
+		return fmt.Sprintf("List<%s>", elemName(tp.Elem()))
+	default:
+		return tp.Elem().Name()
+	}
 }
 
 func MustStructToMap(data interface{}) map[string]interface{} {
